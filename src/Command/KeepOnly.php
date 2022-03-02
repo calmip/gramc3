1025291fb5e340fc4fac351d11c0b2cdc9a6d0cf
Emmanuel Courcelle <emmanuel.courcelle@toulouse-inp.fr>
2022-03-02 14:14:28 +0100

    KeepOnly dérive de Rgpd

--- src/Command/KeepOnly.php
+++ src/Command/KeepOnly.php
@@ -58,18 +58,18 @@ use Doctrine\ORM\EntityManagerInterface;
 //use App\GramcServices\ServiceNotifications;
 
 
-class KeepOnly extends Command
+class KeepOnly extends Rgpd
 {
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'app:keeponly';
 
-    public function __construct(private GramcDate $sd, private ServiceProjets $sp, private ServiceVersions $sv, private ServiceJournal $sj, private EntityManagerInterface $em)
+    public function __construct(protected GramcDate $sd, protected ServiceProjets $sp, protected ServiceVersions $sv, protected ServiceJournal $sj, protected EntityManagerInterface $em)
     {
         // best practices recommend to call the parent constructor first and
         // then set your own properties. That wouldn't work in this case
         // because configure() needs the properties set in this constructor
 
-        parent::__construct();
+        parent::__construct($sd,$sp,$sv,$sj,$em);
     }
 
     protected function configure()
@@ -77,77 +77,6 @@ class KeepOnly extends Command
         $this->setDescription('KeepOnly: Vide presque complètement la base de données: ne garde que quelques projets, et les utilisateurs associés');
         $this->setHelp('Vidage massif de la base de données');
         $this->addArgument('keeponly', InputArgument::REQUIRED, "Projets à conserver: P123456,P234567");
-    }
-
-    // effacer toutes les versions d'un projet
-    private function effacerVersions(Projet $projet, OutputInterface $output)
-    {
-        $em = $this->em;
-        
-        // Effacer les versions
-        foreach ($projet->getVersion() as $version) {
-
-            $output->writeln("                VERSION $version");
-
-            // Effacer les collaborateurs
-            foreach ($version->getCollaborateurVersion() as $item) {
-                $em->remove($item);
-            }
-            $em->flush();
-
-            // Effacer les expertises
-            foreach ($version->getExpertise() as $item) {
-                $em->remove($item);
-            }
-            $em->flush();
-
-            // Effacer les rallonges
-            $this->effacerRallonges($version, $output);
-
-            // Maintenant, on peut effacer la version !
-            // Pour ne pas avoir d'ennuis, effacer d'abord version dernière !
-            $projet->setVersionDerniere(null);
-            $em->persist($projet);
-            $em->flush();
-
-            $em->remove($version);
-            $em->flush();
-        }
-    }
-
-    // effacer toutes les rallonges d'une version
-    private function effacerRallonges(Version $version, OutputInterface $output)
-    {
-        $em = $this->em;
-        
-        // Effacer les rallonges
-        foreach ( $version->getRallonge() as $item)
-        {
-            //$output->writeln("Rallonge $item");
-            $em->remove($item);
-        }
-        $em->flush();
-    }
-
-    // Effacer les documents joints au projet, ainsi que les infos sur le RA
-    // Attention on fait ça AVANT de supprimer les versions !
-    private function effacerDocuments(Projet $projet, OutputInterface $output)
-    {
-        $sv = $this->sv;
-        $sp = $this->sp;
-        $em = $this->em;
-
-        // Pour chaque version, efface les documents joints
-        foreach ($projet->getVersion() as $version) {
-            $sv->effacerDonnees($version);
-        }
-
-        // NOTE - On laisse le rapport d'activité, qui est considéré comme une publication
-        // Donc ce n'est pas une donnée personnelle
-        // Mais on efface les infos sur le rapport d'activité
-        foreach ($projet->getRapportActivite() as $rapport) {
-            $em->remove($rapport);
-        }
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -175,33 +104,9 @@ class KeepOnly extends Command
 
         $allProjets = $em->getRepository(Projet::class)->findAll();
         $mauvais_projets = [];
+        $projets_annee = $this->buildProjetsByYear($anneeLimite, $allProjets, $a_kept);
 
-        // $projets_annee[2015] -> un array contenant la liste des projets arrêtés ou en standby depuis 2015
-        //                on le remplit pour les années des projets à supprimer (<= $anneeAncienne)
-        $projets_annee = [];
-        foreach ($allProjets as $projet) {
-            if (in_array($projet->getIdProjet(),$a_kept))
-            {
-                continue;
-            }
-            $derniereVersion    =  $projet->derniereVersion();
-
-            // Projet merdique - On le met de côté
-            if ($derniereVersion == null) {
-                $mauvais_projets[$projet->getIdProjet()] = $projet;
-                $annee = 0;
-            }
-            else
-            {
-                $annee = $projet->derniereVersion()->getAnneeSession();
-            }
-
-            if (intval($annee) <= $anneeLimite)
-            {
-                $projets_annee[$annee][] = $projet;
-            }
-        }
-
+        // On affiche le tableau $projets_annee
         foreach ($projets_annee as $a => $pAnnee) {
             $output->writeln("");
             $output->writeln("PROJETS TERMINES EN $a");
@@ -211,22 +116,9 @@ class KeepOnly extends Command
             }
         }
 
-        // Rechercher la liste d'utilisateurs correspondant à ces projets
-        $loginnames = [];
-        foreach ($projets_annee as $a => $pAnnee) {
-            foreach ($pAnnee as $p) {
-                //$output->writeln("coucou " . $p->getIdProjet());
-                $loginnames[strtolower($p->getIdProjet()).'-'.$a] = 1;
-                foreach ($p->getVersion() as $v) {
-                    foreach ($v->getCollaborateurVersion() as $cv) {
-                        if ($cv->getLoginname() !== null) {
-                            $loginnames[$cv->getLoginname().'-'.$a] = 1;
-                        }
-                    }
-                }
-            }
-        }
+        $loginnames = $this->buildUsersList($projets_annee);
 
+        // On les affiche
         $output->writeln("");
         $output->writeln("=============================================================================================================");
         $output->writeln("Les enregistrements de compta des groupes ou utilisateurs suivants seront supprimés (loginname - date limite)");
@@ -246,50 +138,11 @@ class KeepOnly extends Command
         // On y va: on commence par écrire dans le journal
         $sj->infoMessage("EXECUTION DE LA COMMANDE: keeponly $kept");
 
-
-        // effacer les données de compta
-        $output->writeln("");
-        $output->writeln("======");
-        $output->writeln("COMPTA");
-        $output->writeln("======");
-        foreach (array_keys($loginnames) as $log)
-        {
-            list($l,$d) = explode('-',$log);
-            if ($d == "0") continue;
-            $annee_limite = intval($a) + 1;
-            $date = new \DateTime("$annee_limite-01-01");
-            $del = $em->getRepository(compta::class)->removeLoginname($l,$date);
-            $output->writeln ("   $log -> lignes supprimées = $del");
-        }
-        
-        foreach ($projets_annee as $a => $pAnnee)
-        {
-            $output->writeln("    ANNEE $a");
-
-            // effacer les données des versions de projets
-            foreach ($projets_annee[$a] as $projet) {
-
-                $output->writeln("        PROJET $projet");
-
-                // effacer les documents joints
-                $output->writeln("                Documents joints");
-                $this->effacerDocuments($projet, $output);
-
-                // effacer les versions du projet
-                $this->effacerVersions($projet, $output);
-                
-                $sj->infoMessage('Le projet ' . $projet . ' a été effacé ');
-
-                $em->remove($projet);
-                $em->flush();
-
-                $output->writeln("                Projet supprimé");
-
-            }
-        }
-        
-        // effacer les utilisateurs qui n'ont pas de projet
+        // effacer les données de compta, les projets
+        $this->effacerCompta($output, $loginnames);
+        $this->effacerProjets($output, $projets_annee);
         $individus_effaces = $sp->effacer_utilisateurs();
+
         $output->writeln("");
         $output->writeln("=================");
         $output->writeln("INDIVIDUS EFFACES");
