b1d51cbb4c43efb3fa388dfdbe1d56d79758609a
Emmanuel Courcelle <emmanuel.courcelle@toulouse-inp.fr>
2022-06-17 16:51:56 +0200

    Réécriture du Javascipt pour les boutons En savoir plus...

--- public/js/base.js
+++ public/js/base.js
@@ -21,39 +21,38 @@ $( document ).ready(function() {
 
 $( document ).ready(function() {
     //Récupérer "En voir plus..." 
-	let more_menu = $('.more')
+    let more_menu = $('.more')
     
-    //Récupérer les li du "En voir plus..." cliqué contenant la classe "priorite2"
-    let prio2 = more_menu.parent().children('.priorite2')
-
-    //Déclaration de la variable flg qui sert à déclarer lorsqu'il y a plus d'un li contenant la classe "priorite2"
-    let flg = false
-    
-    prio2.each(function(){
+    // Dès qu'on a un li.priorite2, on ne l'affiche pas
+    // 
+    $('li.priorite2').each(function(){
         this.style.display = 'none'
-        flg = true
+        let more_locals = $(this).siblings('.more');
+        if (more_locals.length > 0)
+        {
+            more_local = more_locals[0];
+            more_local.style.display = 'block';
+        }
     })
 
-    //S'il y a un li contenant la classe "priorite2", afficher "En voir plus..."
-    if(flg){
-        more_menu.each(function(){this.style.display = 'block'})
-    }
-
-    //Récupérer "En voir plus..." du parent cliqué
-    let ensavoirplus = more_menu.parent().children('.more')
-
-    
-    ensavoirplus.on('click', function(){
-        if(this.classList.contains('more')){ 
-            prio2.each(function(){
+    // Binder les li "En voir plus..."
+    let envoirplus = more_menu.parent().children('.more')
+    envoirplus.on('click', function(){
+        
+        // Bouton En voir plus: on affiche les li de priorité 2 et on le transforme en "en voir moins"
+        if(this.classList.contains('more')){
+            // On travaille sur les éléments de priorité 2 frères de l'élément cliqué
+            $(this).siblings('li.priorite2').each(function(){
                 this.style.display = 'initial'
             })
             this.innerHTML = 'En voir moins...'
             this.classList.add('less')
             this.classList.remove('more')
-        } 
+        }
+
+        // Bouton en voir moins: on cache les li de priorité 2 et on le transforme en "en voir plus"
         else if(this.classList.contains('less')){
-            prio2.each(function(){
+            $(this).siblings('li.priorite2').each(function(){
                 this.style.display = 'none'
             })
             this.innerHTML = 'En voir plus...'
@@ -61,5 +60,4 @@ $( document ).ready(function() {
             this.classList.remove('less')
         }
     })
-	
 })
