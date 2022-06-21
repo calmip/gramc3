d289a47fc0939ca05b3ab2c174849258e1803656
Emmanuel Courcelle <emmanuel.courcelle@toulouse-inp.fr>
2022-06-21 08:23:42 +0200

    Refonte des outils de téléversement

--- public/js/upload.js
+++ public/js/upload.js
@@ -4,35 +4,42 @@
 
 $(document).ready(function() 
 {
-	url=$("#fileuploader").children("a").attr("href");
-	$("#fileuploader").uploadFile(
-	{
-		url: url,
-        fileName: "fichier[fichier]",
-		//returnType: "json",
-		//dynamicFormData: function() 
-		//{
-        //    var data = { "fichier" : {} };
-		//	return data;
-		//},
-		onSuccess:function(files,data,xhr,pd) 
-		{
-			//msg=JSON.stringify(data);
-            msg = data;
-            //alert ( JSON.stringify(msg) );
-			if (msg == 'OK') 
-			{
-				$('#uploadstatus').html("<p>Le fichier est correctement enregistré.</p>").addClass("info").addClass("message");
-				$('#fileuploader').remove();
-				$('#uploadform').remove();
-			} else 
-			{
-				//alert ( JSON.stringify(msg) );
-				$('#uploadstatus').html('<p>'+msg+'</p>').addClass("erreur").addClass("message");
-			}
-		},
-		dragDropStr: "</br><span><b>Faites glisser et déposez le fichier</b></span>",
-		uploadStr:"Téléversez votre description scientifique"
-	});
+	//url=$("#fileuploader").children("a").attr("href");
+    $(".fileuploader").each(function(){
+        let url = $(this).children("a").attr("href");
+        $(this).uploadFile(
+        {
+            url: url,
+            fileName: "fichier[fichier]",
+            //returnType: "json",
+            //dynamicFormData: function() 
+            //{
+            //    var data = { "fichier" : {} };
+            //return data;
+            //},
+            onSuccess:function(files,data,xhr,pd) 
+            {
+                //msg=JSON.stringify(data);
+                msg = data;
+                //alert(data);
+                //alert ( JSON.stringify(msg) );
+                if (msg == 'OK') 
+                {
+                    $('#uploadstatus').html("<p>Le fichier est correctement enregistré.</p>").addClass("ok").addClass("information");
+                    $(this).remove();
+                    //$('#uploadform').remove();
+                }
+                else 
+                {
+                    //alert ( JSON.stringify(msg) );
+                    //$(this).html('<p>'+msg+'</p>').addClass("erreur").addClass("message");
+                    $('#uploadstatus').html("<p>" + msg + "</p>").addClass("error").addClass("information");
+                }
+                $('#uploadstatus').remove();
+            },
+            dragDropStr: "</br><span><b>glissez - déposez</b></span>",
+            uploadStr:"Téléversez"
+        });
+    });
 });
 
