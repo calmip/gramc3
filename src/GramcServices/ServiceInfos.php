96e42a4f47b5deaaf0f4e6e6508d5baa0802f3c5
Emmanuel Courcelle <emmanuel.courcelle@toulouse-inp.fr>
2022-06-24 11:11:01 +0200

    develop = début de la 3.8.0

--- src/GramcServices/ServiceInfos.php
+++ src/GramcServices/ServiceInfos.php
@@ -32,7 +32,7 @@ use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
-const VERSION = "3.7.0dev";
+const VERSION = "3.8.0dev";
 
 /*
  * Cette classe garde des informations pouvant être reprises par
