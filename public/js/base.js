a9041515b489f44db8ec665adefacbe7129a194b
Emmanuel Courcelle <emmanuel.courcelle@toulouse-inp.fr>
2022-07-26 15:14:12 +0200

    BUG - dans base.js: erreru javascipt si pas de lien "afficher plus" dans un menu

--- public/js/base.js
+++ public/js/base.js
@@ -23,48 +23,52 @@ $( document ).ready(function() {
     //Récupérer "En voir plus..." 
     let more_menu = $('.more')
 
-    //Vérifier si le menu est dans la partie admin
-    let container = more_menu[0].parentNode.parentNode.parentNode.parentNode
-        
-    if(container.className != "section_administrateur" && container.className != "section_president"){
-
-        // Dès qu'on a un li.priorite2, on ne l'affiche pas
-        // 
-        $('li.priorite2').each(function(){
-            this.style.display = 'none'
-            let more_locals = $(this).siblings('.more');
-            if (more_locals.length > 0)
-            {
-                more_local = more_locals[0];
-                more_local.style.display = 'block';
-            }
-        })
-
-        // Binder les li "En voir plus..."
-        let envoirplus = more_menu.parent().children('.more')
-        envoirplus.on('click', function(){
+    // Y a-til un ou plusieurs "more menu" ?
+    if (more_menu.length != 0)
+    {
+        let container = more_menu[0].parentNode.parentNode.parentNode.parentNode
             
-            // Bouton En voir plus: on affiche les li de priorité 2 et on le transforme en "en voir moins"
-            if(this.classList.contains('more')){
-                // On travaille sur les éléments de priorité 2 frères de l'élément cliqué
-                $(this).siblings('li.priorite2').each(function(){
-                    this.style.display = 'initial'
-                })
-                this.innerHTML = 'En voir moins...'
-                this.classList.add('less')
-                this.classList.remove('more')
-            }
-            
-            // Bouton en voir moins: on cache les li de priorité 2 et on le transforme en "en voir plus"
-            else if(this.classList.contains('less')){
-                $(this).siblings('li.priorite2').each(function(){
-                    this.style.display = 'none'
-                })
-                this.innerHTML = 'En voir plus...'
-                this.classList.add('more')
-                this.classList.remove('less')
-            }
-        })
+        if(container.className != "section_administrateur" && container.className != "section_president")
+        {
+    
+            // Dès qu'on a un li.priorite2, on ne l'affiche pas
+            // 
+            $('li.priorite2').each(function(){
+                this.style.display = 'none'
+                let more_locals = $(this).siblings('.more');
+                if (more_locals.length > 0)
+                {
+                    more_local = more_locals[0];
+                    more_local.style.display = 'block';
+                }
+            })
+    
+            // Binder les li "En voir plus..."
+            let envoirplus = more_menu.parent().children('.more')
+            envoirplus.on('click', function(){
+                
+                // Bouton En voir plus: on affiche les li de priorité 2 et on le transforme en "en voir moins"
+                if(this.classList.contains('more')){
+                    // On travaille sur les éléments de priorité 2 frères de l'élément cliqué
+                    $(this).siblings('li.priorite2').each(function(){
+                        this.style.display = 'initial'
+                    })
+                    this.innerHTML = 'En voir moins...'
+                    this.classList.add('less')
+                    this.classList.remove('more')
+                }
+                
+                // Bouton en voir moins: on cache les li de priorité 2 et on le transforme en "en voir plus"
+                else if(this.classList.contains('less')){
+                    $(this).siblings('li.priorite2').each(function(){
+                        this.style.display = 'none'
+                    })
+                    this.innerHTML = 'En voir plus...'
+                    this.classList.add('more')
+                    this.classList.remove('less')
+                }
+            })
+        }
     }
 })
 
