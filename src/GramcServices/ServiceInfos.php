3257af48671d7530189724aedb3974cad088d024
Emmanuel Courcelle <emmanuel.courcelle@toulouse-inp.fr>
2022-09-05 11:00:47 +0200

    bump 3.7.3

--- src/GramcServices/ServiceInfos.php
+++ src/GramcServices/ServiceInfos.php
@@ -32,7 +32,7 @@ use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
-const VERSION = "3.7.2";
+const VERSION = "3.7.3 ";
 
 /*
  * Cette classe garde des informations pouvant être reprises par
