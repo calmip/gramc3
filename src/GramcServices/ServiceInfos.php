10560bec11fba3178a6b2c89e66660af14b7bf41
Emmanuel Courcelle <emmanuel.courcelle@toulouse-inp.fr>
2022-09-08 16:29:44 +0200

    Message pas clair lors de l'entrée des collaborateurs

--- src/GramcServices/ServiceInfos.php
+++ src/GramcServices/ServiceInfos.php
@@ -32,7 +32,7 @@ use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
-const VERSION = "3.7.3 ";
+const VERSION = "3.7.4";
 
 /*
  * Cette classe garde des informations pouvant être reprises par
